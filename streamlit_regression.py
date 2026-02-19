7bc65317c2611f085478a4ae27ddca4200fa9598
Vedant Shelake <vedantshelake28@gmail.com>
2026-02-19 21:19:14 +0530

    Regression ANN

---
+++ streamlit_regression.py
@@ -0,0 +1,71 @@
+import pandas as pd 
+import streamlit as st
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler,LabelEncoder
+import pickle
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+import warnings
+warnings.filterwarnings('ignore')
+
+
+model=tf.keras.models.load_model('regression_model.h5')
+
+## load the encoder and scaler
+with open('label_encoder_gender_reg.pkl','rb') as file:
+    label_Encoder_gender=pickle.load(file)
+
+with open('onehot_encoder_geography_reg.pkl','rb') as  file:
+    OneHot_Encoder_geo=pickle.load(file)
+    
+with open('scaler_reg.pkl','rb') as file:
+    scaler=pickle.load(file)
+    
+with open("feature_columns.pkl", "rb") as f:
+    feature_columns = pickle.load(f)
+
+
+## strmlit
+st.title('Customer Churn Prediction')
+
+
+# User input
+geography = st.selectbox('Geography', OneHot_Encoder_geo.categories_[0])
+gender = st.selectbox('Gender', label_Encoder_gender.classes_)
+age = st.slider('Age', 18, 92)
+balance = st.number_input('Balance')
+credit_score = st.number_input('Credit Score')
+exited = st.selectbox('Exited',[0,1])
+tenure = st.slider('Tenure', 0, 10)
+num_of_products = st.slider('Number of Products', 1, 4)
+has_cr_card = st.selectbox('Has Credit Card', [0, 1])
+is_active_member = st.selectbox('Is Active Member', [0, 1])
+
+
+
+# Prepare the input data
+input_data = pd.DataFrame({
+    'CreditScore': [credit_score],
+    'Gender': [label_Encoder_gender.transform([gender])[0]],
+    'Age': [age],
+    'Tenure': [tenure],
+    'Balance': [balance],
+    'NumOfProducts': [num_of_products],
+    'HasCrCard': [has_cr_card],
+    'IsActiveMember': [is_active_member],
+    'Exited': [exited]
+})
+
+geo_encoded = OneHot_Encoder_geo.transform([[geography]]).toarray()
+geo_df=pd.DataFrame(geo_encoded,columns=OneHot_Encoder_geo.get_feature_names_out(['Geography']))
+
+
+input_data=pd.concat([input_data.reset_index(drop=True),geo_df],axis=1)
+input_data_scaled=scaler.transform(input_data)
+
+predication=model.predict(input_data_scaled)
+predication_proba=predication[0][0]
+
+st.write(f' Estimated Salary Probability:{predication_proba:.2f}')
+
